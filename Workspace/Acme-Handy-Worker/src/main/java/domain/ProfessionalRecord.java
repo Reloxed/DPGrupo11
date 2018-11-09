79184db3b217912d32df4895376c2160eaaedaa0
cramersito <carlosalbalat97@gmail.com>
2018-11-09 16:28:22 +0100

    Persistencia Java

--- Workspace/Acme-Handy-Worker/src/main/java/domain/ProfessionalRecord.java
+++ Workspace/Acme-Handy-Worker/src/main/java/domain/ProfessionalRecord.java
@@ -2,12 +2,17 @@ package domain;
 
 import java.sql.Date;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
+@Entity
+@Access(AccessType.PROPERTY)
 public class ProfessionalRecord extends DomainEntity {
 
 	private String companyName;
