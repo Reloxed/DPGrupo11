a9b5d5acec090cd1d7cc02a55006ba4c73435af7
luciadcf <luciadelcarmenfuentes@gmail.com>
2018-11-21 17:13:45 +0100

    Updating ApplicationService

--- Workspace/Acme-Handy-Worker/src/main/java/services/ApplicationService.java
+++ Workspace/Acme-Handy-Worker/src/main/java/services/ApplicationService.java
@@ -1,6 +1,7 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
@@ -11,7 +12,6 @@ import org.springframework.util.Assert;
 
 import repositories.ApplicationRepository;
 import domain.Application;
-import domain.CreditCard;
 import domain.FixUpTask;
 import domain.HandyWorker;
 
@@ -22,10 +22,15 @@ public class ApplicationService {
 	// Managed repository -----------------------
 
 	@Autowired
-	private ApplicationRepository	applicationRepository;
+	private ApplicationRepository		applicationRepository;
+
+	// Supporting services --------------------------
 
 	@Autowired
-	private HandyWorkerService		handyWorkerService;
+	private HandyWorkerService			handyWorkerService;
+
+	@Autowired
+	private SystemConfigurationService	systemConfigurationService;
 
 
 	// Handy Worker : List, show, create
@@ -48,28 +53,66 @@ public class ApplicationService {
 	public Application save(final Application a) {
 		final HandyWorker applicant;
 		final Application result;
-		final CreditCard creditCard;
 		final Date registeredMoment;
 		final FixUpTask fixUpTask;
 		final String messageHandyWorker;
 		final String messageCustomer;
+		final Collection<Application> applications, updated;
+		final Collection<String> spamWords;
+		final boolean containsSpam;
 
 		Assert.notNull(a);
 		applicant = this.handyWorkerService.findByPrincipal();
 		Assert.notNull(applicant);
 
 		fixUpTask = a.getFixUpTask();
-		result = null;
 		if (a.getId() == 0) { // Not saved in database yet
 			a.setStatus("PENDING");
 			registeredMoment = new Date(System.currentTimeMillis() - 1);
 			Assert.isTrue(registeredMoment.after(fixUpTask.getPublishedMoment())); // The fixUpTask must be published.
-
-			//creditCard = 
+			a.setRegisteredMoment(registeredMoment);
+			a.setComment(new String());
 		}
+
+		a.setApplicant(applicant);
+
+		result = this.applicationRepository.save(a);
+		Assert.notNull(a);
+
+		// Add application to collection of applications of handyWorker
+		applications = applicant.getApplications();
+		updated = new ArrayList<Application>(applications);
+		updated.add(result);
+		applicant.setApplications(updated);
+
+		// Check contain of strings searching spamWords
+		/*
+		 * containsSpam = false;
+		 * spamWords = this.systemConfigurationService
+		 */
+
+		// TODO: necesito un metodo de systemConfiguration que me devuelva las palabras spam para comprobar que el contenido de los comentarios no tiene spam
+
+		// TODO: falta el mensaje de notificacion
+		return result;
+	}
+
+	public Collection<Application> findAll() {
+		Collection<Application> result;
+
+		result = this.applicationRepository.findAll();
 
 		return result;
 	}
+
+	public Application findOne(final int applicationId) {
+		Application result;
+
+		result = this.applicationRepository.findOne(applicationId);
+
+		return result;
+	}
+
 	// Other business methods --------------------------
 
 	public Collection<Application> findAllApplicationsByHandyWorker(final int handyWorkerId) {
