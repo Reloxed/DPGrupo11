2a4fbb680ff5a79398b482921cdd2a908f8955c5
luciadcf <luciadelcarmenfuentes@gmail.com>
2018-11-21 16:42:09 +0100

    Application use cases

--- Workspace/Acme-Handy-Worker/src/main/java/repositories/ApplicationRepository.java
+++ Workspace/Acme-Handy-Worker/src/main/java/repositories/ApplicationRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Application;
@@ -9,4 +12,24 @@ import domain.Application;
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
+	@Query("select a from HandyWorker h join h.applications a where h.id=?1")
+	Collection<Application> findAllApplicationsByHandyWorker(int handyWorkerId);
+
+	@Query("select a from FixUpTask f join f.applications a where f.id=?1 order by a.status")
+	Collection<Application> findAllApplicationsByFixUpTask(int fixUptaskId);
+
+	@Query("select (sum(case when a.status='PENDING' then 1.0 else 0 end)/count(*)) from Application a")
+	Double ratioPendingApplications();
+
+	@Query("select (sum(case when a.status='ACCEPTED' then 1.0 else 0 end)/count(*)) from Application a")
+	Double ratioAcceptedApplications();
+
+	@Query("select (sum(case when a.status='REJECTED' then 1.0 else 0 end)/count(*)) from Application a")
+	Double ratioRejectedApplications();
+
+	@Query("select count(a)/(select count(a) from Application a where a.fixUpTask.endMoment < CURRENT_DATE and a.status='PENDING')*1.0 from Application a where a.status='PENDING'")
+	Double ratioPendingApplicationsElapsedPeriod();
+
+	@Query("select avg(a.offeredPrice), max(a.offeredPrice), min(a.offeredPrice), sqrt(sum(a.offeredPrice*a.offeredPrice)/count(a.offeredPrice)-(avg(a.offeredPrice)*avg(a.offeredPrice))) from Application a group by 'a'")
+	Double[] findDataApplicationsOfferedPrice();
 }
--- Workspace/Acme-Handy-Worker/src/main/java/services/ApplicationService.java
+++ Workspace/Acme-Handy-Worker/src/main/java/services/ApplicationService.java
@@ -1,11 +1,131 @@
 
 package services;
 
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.ApplicationRepository;
+import domain.Application;
+import domain.CreditCard;
+import domain.FixUpTask;
+import domain.HandyWorker;
 
 @Service
 @Transactional
 public class ApplicationService {
+
+	// Managed repository -----------------------
+
+	@Autowired
+	private ApplicationRepository	applicationRepository;
+
+	@Autowired
+	private HandyWorkerService		handyWorkerService;
+
+
+	// Handy Worker : List, show, create
+	// Customer : List, update
+
+	// Simple CRUD methods ---------------------------
+	public Application create() {
+		final Application result;
+		HandyWorker applicant;
+
+		applicant = this.handyWorkerService.findByPrincipal();
+		Assert.notNull(applicant);
+
+		result = new Application();
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	public Application save(final Application a) {
+		final HandyWorker applicant;
+		final Application result;
+		final CreditCard creditCard;
+		final Date registeredMoment;
+		final FixUpTask fixUpTask;
+		final String messageHandyWorker;
+		final String messageCustomer;
+
+		Assert.notNull(a);
+		applicant = this.handyWorkerService.findByPrincipal();
+		Assert.notNull(applicant);
+
+		fixUpTask = a.getFixUpTask();
+		result = null;
+		if (a.getId() == 0) { // Not saved in database yet
+			a.setStatus("PENDING");
+			registeredMoment = new Date(System.currentTimeMillis() - 1);
+			Assert.isTrue(registeredMoment.after(fixUpTask.getPublishedMoment())); // The fixUpTask must be published.
+
+			//creditCard = 
+		}
+
+		return result;
+	}
+	// Other business methods --------------------------
+
+	public Collection<Application> findAllApplicationsByHandyWorker(final int handyWorkerId) {
+		Collection<Application> result;
+
+		result = this.applicationRepository.findAllApplicationsByHandyWorker(handyWorkerId);
+
+		return result;
+	}
+
+	public Collection<Application> findAllApplicationsByFixUpTask(final int fixUptaskId) {
+		Collection<Application> result;
+
+		result = this.applicationRepository.findAllApplicationsByFixUpTask(fixUptaskId);
+
+		return result;
+	}
+
+	public Double ratioPendingApplications() {
+		final Double result;
+
+		result = this.applicationRepository.ratioPendingApplications();
+
+		return result;
+	}
+
+	public Double ratioAcceptedApplications() {
+		Double result;
+
+		result = this.applicationRepository.ratioAcceptedApplications();
+
+		return result;
+	}
+
+	public Double ratioRejectedApplications() {
+		Double result;
+
+		result = this.applicationRepository.ratioRejectedApplications();
+
+		return result;
+	}
+
+	public Double ratioPendingApplicationsElapsedPeriod() {
+		Double result;
+
+		result = this.applicationRepository.ratioPendingApplicationsElapsedPeriod();
+
+		return result;
+	}
+
+	public Double[] findDataApplicationsOfferedPrice() {
+		Double[] result;
+
+		result = this.applicationRepository.findDataApplicationsOfferedPrice();
+
+		return result;
+	}
 
 }
