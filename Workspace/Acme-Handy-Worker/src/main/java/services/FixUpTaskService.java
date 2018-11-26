db7effc678fb6390b4675198c49c744eab750a12
FraPazGal <frandepaz@yahoo.es>
2018-11-26 16:37:17 +0100

    Revert "Revert "Merge branch 'master' into Fran""
    This reverts commit a87ccc3470991aef07ccb20d4eaba84438fe2130.

--- Workspace/Acme-Handy-Worker/src/main/java/services/FixUpTaskService.java
+++ Workspace/Acme-Handy-Worker/src/main/java/services/FixUpTaskService.java
@@ -1,13 +1,21 @@
 package services;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 
+import domain.Application;
+import domain.Category;
+import domain.Customer;
 import domain.FixUpTask;
+import domain.Warranty;
 
 import repositories.FixUpTaskRepository;
 
@@ -24,6 +32,11 @@ public class FixUpTaskService {
 	
 	
 	//Supporting services ----------
+	@Autowired 
+	private CurriculumService curriculumService;
+	
+	@Autowired
+	private CustomerService cusotmerService;
 	
 	
 	//Constructor ----------------------------------------------------
@@ -33,29 +46,78 @@ public class FixUpTaskService {
 	
 	
 	//Simple CRUD methods-------
-	//teniendo en cuenta las reglas de negocio
 	
-	public FixUpTask create(){
-		return null;
+	
+	public FixUpTask create(final Category category,final Warranty warranty){
+		FixUpTask result;
+		Date currentMoment;
+		
+		result=new FixUpTask();
+		result.setPublishedMoment(new Date(System.currentTimeMillis() - 1));
+		result.setApplications(new HashSet<Application>());
+		result.setCategory(category);
+		result.setTicker(this.curriculumService.generateTicker());
+		result.setWarranty(warranty);
+		//falta quien la crea?
+
+		
+		
+		return result;
 		
 	}
 	public Collection<FixUpTask> findAll(){
-		return null;
+		Collection<FixUpTask> result;
+
+		result = this.fixUpTaskRepository.findAll();
+		Assert.notNull(result);
+		
+		return result;
 		
 	} 
 	
 	public FixUpTask findOne(int fixUpTaskId){
-		return null;
+		FixUpTask result;
+
+		result = this.fixUpTaskRepository.findOne(fixUpTaskId);
+		Assert.notNull(result);
+
+		return result;
 		
 	}
 	
 	public FixUpTask save(FixUpTask fixUpTask){
-		return null;
+		FixUpTask result;
+		Customer principal;
+		
+		Assert.isTrue(fixUpTask.getApplications().isEmpty());
+		Assert.isTrue(fixUpTask.getWarranty().getIsFinal());
+		Assert.isTrue(fixUpTask.getId()!=0);
+		Assert.isTrue(fixUpTask.getStartMoment().before(fixUpTask.getEndMoment()));
+		
+		principal=this.cusotmerService.findByPrincipal();
+		Assert.notNull(principal);
+		
+		
+		result=this.fixUpTaskRepository.save(fixUpTask);
+		
+		return result;
+		
 		
 	}
 	
 	public void  delete(FixUpTask fixUpTask){
-		return;
+		Customer principal;
+		
+		Assert.notNull(fixUpTask);
+		Assert.notNull(fixUpTask.getId()!=0);
+		
+		principal=this.cusotmerService.findByPrincipal();
+		Assert.notNull(principal);
+		
+		Assert.isTrue(fixUpTask.getApplications().isEmpty());//no se puede eliminar una chapuza si tiene solicitudes
+		
+		this.fixUpTaskRepository.delete(fixUpTask);		
+		
 		
 	}
 	
