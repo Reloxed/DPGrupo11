ef9895d3164c88849ca7b40ab2304f4ef3e4d240
Reloxed <antoniomma97@gmail.com>
2018-11-29 16:09:33 +0100

    SystemConfigurationService test casi terminado, sponsorshipServiceTest acabado.

--- Workspace/Acme-Handy-Worker/src/main/java/services/SocialProfileService.java
+++ Workspace/Acme-Handy-Worker/src/main/java/services/SocialProfileService.java
@@ -2,11 +2,11 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.SocialProfileRepository;
