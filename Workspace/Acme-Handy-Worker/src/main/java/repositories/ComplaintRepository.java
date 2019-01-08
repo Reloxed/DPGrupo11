b4cdd7054de4b017ac8a109b5320e9c4a396c99f
Reloxed <antoniomma97@gmail.com>
2019-01-08 17:15:11 +0100

    Intento numero 2

--- Workspace/Acme-Handy-Worker/src/main/java/repositories/ComplaintRepository.java
+++ Workspace/Acme-Handy-Worker/src/main/java/repositories/ComplaintRepository.java
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-
-package repositories;
-
-import java.util.Collection;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import domain.Complaint;
-
-@Repository
-public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
-	
-	@Query("select c from HandyWorker h join h.applications a join a.fixUpTask f join f.complaints c where h.id=?1 group by c.id")
-	Collection<Complaint> findComplaintsByHandyWorkerId(int handyWorkerId);
-	
-}
-=======
-
 package repositories;
 
 import java.util.Collection;
@@ -40,4 +19,4 @@ public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
 	Collection<Complaint> findComplaintsByHandyWorkerId(int handyWorkerId);
 
 }
->>>>>>> 236ca83ac84cad3bbae634252eb077ccbd3adb7c
+
