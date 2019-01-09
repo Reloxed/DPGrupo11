fc47dbaad131c52e378441f6d73205670f49b6d6
Reloxed <antoniomma97@gmail.com>
2019-01-09 18:22:15 +0100

    ComplaintCustomerController terminado.

--- Workspace/Acme-Handy-Worker/src/main/java/controllers/customer/ComplaintCustomerController.java
+++ Workspace/Acme-Handy-Worker/src/main/java/controllers/customer/ComplaintCustomerController.java
@@ -2,17 +2,21 @@ package controllers.customer;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import domain.Complaint;
-
 import services.ComplaintService;
 import services.CustomerService;
+import domain.Complaint;
+import domain.FixUpTask;
 
 @Controller
 @RequestMapping("/complaint/customer")
@@ -41,6 +45,74 @@ public class ComplaintCustomerController {
 		return result;
 	}
 
-	// 
+	// Display
 	
+	
+	
+	// Create
+	
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public ModelAndView create(){
+		ModelAndView result;
+		Complaint complaint;
+		
+		complaint = this.complaintService.create();
+		result = this.createEditModelAndView(complaint);
+		
+		return result;
+	}
+	
+	@RequestMapping(value="/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam int complaintId){
+		ModelAndView result;
+		Complaint complaint;
+		
+		complaint = this.complaintService.findOne(complaintId);
+		Assert.notNull(complaint);
+		
+		result = this.createEditModelAndView(complaint);
+		
+		return result;
+	}
+	
+	@RequestMapping(value="/edit", method = RequestMethod.POST, params="save")
+	public ModelAndView save(@Valid Complaint complaint, BindingResult binding){
+		ModelAndView result;
+		
+		if(binding.hasErrors()){
+			result = createEditModelAndView(complaint);
+		} else {
+			try {
+				this.complaintService.save(complaint);
+				result = new ModelAndView("redirect:list.do");
+			}catch(Throwable oops){
+				result = this.createEditModelAndView(complaint, "complaint.commit.error");
+			}
+		}
+		return result;
+	}
+	
+	// Ancillary methods
+	
+	protected ModelAndView createEditModelAndView(Complaint complaint){
+		ModelAndView result;
+		
+		result = this.createEditModelAndView(complaint, null);
+		
+		return result;
+	}
+	
+	protected ModelAndView createEditModelAndView(Complaint complaint, String message){
+		ModelAndView result;
+		Collection<FixUpTask> listFixUpTasks;
+		
+		listFixUpTasks = this.customerService.findByPrincipal().getFixUpTasks();
+		
+		result = new ModelAndView("complaint/edit");
+		result.addObject("complaint", complaint);
+		result.addObject("message", message);
+		result.addObject("listFixUpTasks", listFixUpTasks);
+		
+		return result;
+	}
 }
--- Workspace/Acme-Handy-Worker/src/main/webapp/views/master-page/header.jsp
+++ Workspace/Acme-Handy-Worker/src/main/webapp/views/master-page/header.jsp
@@ -92,9 +92,9 @@
 						code="master.page.customer.complaints" /></a>
 				<ul>
 					<li class="arrow"></li>
-					<li><a href="complaint/list.do"><spring:message
+					<li><a href="complaint/customer/list.do"><spring:message
 								code="master.page.customer.complaints.show" /></a></li>
-					<li><a href="complaint/edit.do"><spring:message
+					<li><a href="complaint/customer/create.do"><spring:message
 								code="master.page.customer.complaints.create" /></a></li>
 				</ul></li>
 			<li><a class="fNiv"><spring:message
